36bafdc9a407540a585cb79674d1e21941ea70be
suuuuuuminnnnnn <sumin45402214@gmail.com>
2026-04-29 14:03:52 +0900

    feat: PostgreSQL, Docker Compose, Flyway 마이그레이션 설정 추가

--- src/main/kotlin/com/liveklass/notification/NotificationServerApplication.kt
+++ src/main/kotlin/com/liveklass/notification/NotificationServerApplication.kt
@@ -2,8 +2,10 @@ package com.liveklass.notification
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class NotificationServerApplication
 
 fun main(args: Array<String>) {
