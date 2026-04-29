29d8fbc3c7658533cfa317af29d2e94f037dcca2
suuuuuuminnnnnn <sumin45402214@gmail.com>
2026-04-29 13:44:32 +0900

    init: 프로젝트 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,16 +19,33 @@ repositories {
 	mavenCentral()
 }
 
+val kotestVersion = "5.9.1"
+val mockkVersion = "1.13.12"
+val kotestSpringVersion = "1.3.0"
+val testcontainersVersion = "1.20.4"
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-database-postgresql")
+
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 	runtimeOnly("org.postgresql:postgresql")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	}
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("org.testcontainers:postgresql:$testcontainersVersion")
+	testImplementation("org.testcontainers:junit-jupiter:$testcontainersVersion")
+	testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+	testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:$kotestSpringVersion")
+	testImplementation("io.mockk:mockk:$mockkVersion")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
